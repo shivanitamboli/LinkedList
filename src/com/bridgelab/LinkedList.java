a360be7e0110db5dac4855b5d7f5771665e1102d
[shivani tamboli] <[tambolishivani4@gmail.com]>
2021-10-09 12:26:35 +0530

    Added : pop last elements in linked list

--- src/com/bridgelab/LinkedList.java
+++ src/com/bridgelab/LinkedList.java
@@ -1,4 +1,5 @@
 package com.bridgelab;
+
 public class LinkedList {
     public static void main(String[] args) {
         MyNode<Integer> myFirstNode = new MyNode<>(56);
@@ -9,8 +10,8 @@ public class LinkedList {
         myLinkedList.append(myThirdNode);
         myLinkedList.insertInMiddle(myFirstNode,mySecondNode);
         myLinkedList.displayNode();
-        //To delete the first element in the sequence
-        myLinkedList.popFirstNode();
+        //To delete the last element in the sequence
+        myLinkedList.popLastNode();
         myLinkedList.displayNode();
     }
 }
--- src/com/bridgelab/MyLinkedList.java
+++ src/com/bridgelab/MyLinkedList.java
@@ -1,89 +1,99 @@
 package com.bridgelab;
-/**
- * Purpose - To create methods for Linked List operation.
- */
+
 public class MyLinkedList {
-    public INode head;
-    public INode tail;
+	public INode head;
+	public INode tail;
 
-    public MyLinkedList() {
-        this.head = null;
-        this.tail = null;
-    }
+	public MyLinkedList() {
+		this.head = null;
+		this.tail = null;
+	}
 
-    /**
-     * This method is used to add elements to the LinkedList
-     */
-    public void add(INode newNode) {
-        if (this.tail == null) {
-            this.tail = newNode;
-        }
+	/**
+	 * This method is used to add elements to the LinkedList
+	 */
+	public void add(INode newNode) {
+		if (this.tail == null) {
+			this.tail = newNode;
+		}
 
-        if (this.head == null) {
-            this.head = newNode;
-        } else {
-            INode tempNode = head;
-            head = newNode;
-            head.setNext(tempNode);
-        }
-    }
+		if (this.head == null) {
+			this.head = newNode;
+		} else {
+			INode tempNode = head;
+			head = newNode;
+			head.setNext(tempNode);
+		}
+	}
 
-    /**
-     * This method is used to append elements to the LinkedList
-     */
+	/**
+	 * This method is used to append elements to the LinkedList
+	 */
 
-    public void append(INode newNode) {
-        if (this.tail == null)
-            this.tail = newNode;
+	public void append(INode newNode) {
+		if (this.tail == null)
+			this.tail = newNode;
 
-        if (this.head == null)
-            this.head = newNode;
+		if (this.head == null)
+			this.head = newNode;
 
-        else {
-            tail.setNext(newNode);
-            tail=newNode;
+		else {
+			tail.setNext(newNode);
+			tail = newNode;
 
-        }
-    }
+		}
+	}
 
-    /**
-     * This method is used to insert elements in between two elements in the LinkedList
-     */
+	/**
+	 * This method is used to insert elements in between two elements in the
+	 * LinkedList
+	 */
 
-    public void insertInMiddle(MyNode myNode, MyNode newNode) {
-        INode tempNode = myNode.getNext();
-        myNode.setNext(newNode);
-        newNode.setNext(tempNode);
-    }
-    /**
-     *  Method to delete first node in LinkedList
-     */
-    public void popFirstNode() {
-        if(head == null)
-            System.out.println("Linked List is empty!");
-        else
-            head = head.getNext();
-    }
+	public void insertInMiddle(MyNode myNode, MyNode newNode) {
+		INode tempNode = myNode.getNext();
+		myNode.setNext(newNode);
+		newNode.setNext(tempNode);
+	}
 
-    /**
-     * This method is used to display the sequence
-     */
+	/**
+	 * Method to delete first node in LinkedList
+	 */
+	public void popFirstNode() {
+		if (head == null)
+			System.out.println("Linked List is empty!");
+		else
+			head = head.getNext();
+	}
 
-    public void displayNode () {
-        INode current = head;
+	/**
+	 * Method to delete last element in the LinkedList
+	 */
+	public void popLastNode() {
+		INode tempNode = head;
+		while (tempNode.getNext() != tail) {
+			tempNode = tempNode.getNext();
+		}
+		tempNode.setNext(null);
+		tail = tempNode;
+	}
 
-        System.out.print("Nodes are: ");
-        while (current != null) {
-            if (current.getNext() != null) {
-                System.out.print(current.getKey() + " - ");
-                current = current.getNext();
-            } else {
-                System.out.println(current.getKey());
-                current = current.getNext();
-            }
-        }
+	/**
+	 * This method is used to display the sequence
+	 */
 
-    }
+	public void displayNode() {
+		INode current = head;
+
+		System.out.print("Nodes are: ");
+		while (current != null) {
+			if (current.getNext() != null) {
+				System.out.print(current.getKey() + " - ");
+				current = current.getNext();
+			} else {
+				System.out.println(current.getKey());
+				current = current.getNext();
+			}
+		}
+
+	}
 }
-
-
